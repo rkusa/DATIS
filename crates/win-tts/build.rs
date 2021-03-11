3e66d5003a31742839857cc7538680e5e9ad493e
Markus Ast <m@rkusa.st>
2021-03-11 18:29:22 +0100

    upgrade windows crate to 0.4

--- crates/win-tts/build.rs
+++ crates/win-tts/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     #[cfg(target_os = "windows")]
     windows::build!(
+        windows::foundation::*,
         windows::storage::streams::*,
         windows::media::speechsynthesis::*,
     );
